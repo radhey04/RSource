ddcae7827979e3e4f576bcccf5da1ed942aa5720
radhey04 <radhey04@gmail.com>
2014-01-23 13:11:41 +0530

    Update exp1.R

--- exp1.R
+++ exp1.R
@@ -42,3 +42,37 @@ am1 <- am[gd,][,]
 ad1 <- as.numeric(am[ ,1])
  as1 <- as.character(am[ ,2])
  boxplot(ad1~as1, data=am1)
+
+
+ # Part 4:
+ data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+ d2 <- data[data$State=="AL", ]
+ #class(d2[3,11])
+ d2[ ,11] <- as.numeric(as.character(d2[ ,11]))
+ #class(d2[4,11])
+ #d2[1,c(7,11,17,23)]
+ #class(d2)
+ 
+ #d3 <- d2[, c(7,11,17,23)]
+ 
+ #d3[4,c(1,2,3)]
+ #d3[ ,c(2,3,4,)] <- as.numeric(d3[ ,c(2,3,4)])
+ d4 <- d2[, c(7,11)]
+ d4[4:8,c(1,2)]
+ #d4[,2] <- as.numeric(d4[,2])
+ #d4[4,c(1,2)]
+ gd4 <- complete.cases(d4)
+ gd4[4:8]
+ d4 <- d4[gd4,]
+ d4[1:5,c(1,2)]
+ #d4
+ #d4[ ,2] <- as.numeric(d4[,2])
+ #length(d5)
+ #gd1 <- d4[ , 2]
+ #gd2 <- gd1 != "Not Available"
+ #d6 <- d4[gd2,][,]
+ #d6[ ,2] <- as.numeric(d6[,2])
+ #nrow(d6)
+ d7 <- d4[order(d4[ ,2]), ]
+ d7[,2]
+ min(d7[ ,2])
